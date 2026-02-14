bd140469bcd7dd5a0ecdfe1b213c6d69ee15d077
Edreione <edrei.elias@skdymexico.com>
2026-02-14 15:48:32 -0600

    Update api.py

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 api_router = APIRouter()
 
 from app.modules.auth.router import router as auth_router
+from app.modules.inventory.router import router as inventory_router
 
 api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
+api_router.include_router(inventory_router, prefix="/inventory", tags=["inventory"])
 
 @api_router.get("/")
 async def root():
